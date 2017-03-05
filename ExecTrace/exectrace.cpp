54fefa9c9fbab851926e16b51d416a99e20e20e8
rj2394@columbia.edu <rj2394@columbia.edu>
2017-03-05 15:43:12 -0500

    revise exectrace

--- ExecTrace/exectrace.cpp
+++ ExecTrace/exectrace.cpp
@@ -143,7 +143,7 @@ int main(int argc, char * argv[]) {
     IMG_AddInstrumentFunction(ImageRoutine, 0);
 
     TRACE_AddInstrumentFunction(TraceRoutine, 0);
-    RTN_AddInstrumentFunction(InstrRoutine, 0);
+//    RTN_AddInstrumentFunction(InstrRoutine, 0);
 
     PIN_AddFiniFunction(Fini, 0);
 
