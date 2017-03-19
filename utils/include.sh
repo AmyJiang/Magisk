b8fd6804b13b6328d1cadaccd110dba420c34a2b
HidyHan <yh2635@columbia.edu>
2017-03-19 16:25:15 -0400

    config

--- utils/include.sh
+++ utils/include.sh
@@ -1,6 +1,6 @@
-BUILD_LIBS=/home/amy/Documents/Spring2017/OS/magisk/build
-SRC_LIBS=/home/amy/Documents/Spring2017/OS/magisk/libs
-PATCHES=/home/amy/Documents/Spring2017/OS/magisk/patches
+BUILD_LIBS=~/Documents/magisk/build
+SRC_LIBS=~/Documents/magisk/libs
+PATCHES=~/Documents/magisk/patches
 
 OPENSSL=openssl
 LIBRESSL=libressl
