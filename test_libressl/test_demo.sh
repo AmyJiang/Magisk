e85e4c07658fd0b399a24277f3439d39bedfbc26
rj2394@columbia.edu <rj2394@columbia.edu>
2017-03-25 16:50:43 -0400

    clean up test_libressl

--- test_libressl/test_demo.sh
+++ test_libressl/test_demo.sh
@@ -4,12 +4,12 @@ RESET=`tput sgr0`
 
 echo "${HL}[+] Building Pintool ExecTrace"
 pushd ../ExecTrace > /dev/null
-    make clean > /dev/null 2>1
-    make > /dev/null 2>1
+    make clean > /dev/null 2>&1
+    make > /dev/null 2>&1
 popd >/dev/null
 
 echo "${HL}[+] Building test_libressl${RESET}"
-make clean >/dev/null 2>1
+make clean >/dev/null 2>&1
 make >/dev/null
 
 echo "${HL}[+] Testing on two certificates: valid.der and invalid.der${RESET}"
