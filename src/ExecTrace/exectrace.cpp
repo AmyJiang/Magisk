e4e8ff1b26ad2e65246c2790302d7a413f512a83
rj2394@columbia.edu <rj2394@columbia.edu>
2017-04-23 12:28:40 -0400

    pintool: add mem trace

--- src/ExecTrace/exectrace.cpp
+++ src/ExecTrace/exectrace.cpp
@@ -1,127 +1,185 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <string>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <sys/mman.h>
 #include <unistd.h>
 #include <vector>
-#include <sys/mman.h>
 
 #include "pin.H"
 
-static const std::string blacklisted_imgs_[] = {
-    "/lib64/ld-linux-x86-64.so.2",
-    "/usr/lib/x86_64-linux-gnu/libstdc++.so.6",
-    "/lib/x86_64-linux-gnu/libm.so.6",
-    "/lib/x86_64-linux-gnu/libgcc_s.so.1",
-    "/lib/x86_64-linux-gnu/libc.so.6"
-};
+std::ofstream TraceFile;
 
-static std::vector<std::string>  blacklisted_imgs(blacklisted_imgs_, blacklisted_imgs_+sizeof(blacklisted_imgs_) / sizeof(std::string));
-std::vector<std::pair<ADDRINT, ADDRINT> > regions;
+KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o",
+                            "exectrace.out",
+                            "specify trace (memory access) file name");
+
+KNOB<BOOL> KnobMem(KNOB_MODE_WRITEONCE, "pintool", "mem", "0",
+                   "output memory access trace");
+
+static const std::string blacklisted_imgs_[] = {
+    "/lib64/ld-linux-x86-64.so.2", "/usr/lib/x86_64-linux-gnu/libstdc++.so.6",
+    "/lib/x86_64-linux-gnu/libm.so.6", "/lib/x86_64-linux-gnu/libgcc_s.so.1",
+    "/lib/x86_64-linux-gnu/libc.so.6"};
+
+static std::vector<std::string> blacklisted_imgs(blacklisted_imgs_,
+                                                 blacklisted_imgs_ +
+                                                     sizeof(blacklisted_imgs_) /
+                                                         sizeof(std::string));
+std::vector<std::pair<ADDRINT, ADDRINT>> regions;
 
 const int kOutFd = 3;
 const int kMaxOutput = 1 << 24;
-uint64_t* output_data = NULL;
-uint64_t* output_pos  = NULL;
+uint64_t *output_data = NULL;
+uint64_t *output_pos = NULL;
 uint64_t total = 0;
 
+static VOID RecordMem(VOID *ip, CHAR r, VOID *addr, INT32 size) {
+  TraceFile << r << " " << ip << " " << addr << " " << size << endl;
+}
+
+static VOID *WriteAddr;
+static INT32 WriteSize;
+
+static VOID RecordWriteAddrSize(VOID *addr, INT32 size) {
+  WriteAddr = addr;
+  WriteSize = size;
+}
+
+static VOID RecordMemWrite(VOID *ip) {
+  RecordMem(ip, 'W', WriteAddr, WriteSize);
+}
 
 VOID ImageRoutine(IMG img, VOID *v) {
-    std::string name = IMG_Name(img);
-    if (!IMG_IsMainExecutable(img)) {
-        return;
-    }
-    if (std::find(blacklisted_imgs.begin(), blacklisted_imgs.end(), name) != blacklisted_imgs.end()) {
-        return;
-    }
-    regions.push_back(make_pair(IMG_LowAddress(img), IMG_HighAddress(img)));
+  std::string name = IMG_Name(img);
+  if (!IMG_IsMainExecutable(img)) {
+    return;
+  }
+  if (std::find(blacklisted_imgs.begin(), blacklisted_imgs.end(), name) !=
+      blacklisted_imgs.end()) {
+    return;
+  }
+  regions.push_back(make_pair(IMG_LowAddress(img), IMG_HighAddress(img)));
 }
-
 
 BOOL ValidAddr(ADDRINT addr) {
-    for (unsigned int i = 0; i < regions.size(); i++) {
-        if (addr >= regions[i].first && addr <= regions[i].second) {
-            return true;
-        }
+  for (unsigned int i = 0; i < regions.size(); i++) {
+    if (addr >= regions[i].first && addr <= regions[i].second) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
-
 
 void write_output(uint64_t v) {
-    if (output_pos < output_data || (char*)output_pos >= (char*)output_data + kMaxOutput) {
-        std::cerr << "output overflowed" << std::endl;
-        return;
-    }
-    *output_pos = v;
-    output_pos++;
+  if (output_pos < output_data ||
+      (char *)output_pos >= (char *)output_data + kMaxOutput) {
+    std::cerr << "output overflowed" << std::endl;
+    return;
+  }
+  *output_pos = v;
+  output_pos++;
 }
 
-
-
-VOID PIN_FAST_ANALYSIS_CALL handle_bb(UINT32 num_instr, ADDRINT address) {
-    write_output(address);
-    total++;
+VOID PIN_FAST_ANALYSIS_CALL RecordBBL(VOID *ip, UINT32 size) {
+  write_output((uint64_t)ip);
+  total++;
+  TraceFile << "B" << " " << ip << " " << size << endl;
 }
-
 
 VOID TraceRoutine(TRACE trace, VOID *v) {
-    if (!ValidAddr(TRACE_Address(trace))) return;
+  if (!ValidAddr(TRACE_Address(trace)))
+    return;
 
-    for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl)) {
-        BBL_InsertCall(
-            bbl,
-            IPOINT_BEFORE,
-            (AFUNPTR)handle_bb,
-            IARG_FAST_ANALYSIS_CALL,
-            IARG_UINT32,
-            BBL_NumIns(bbl),
-            IARG_ADDRINT,
-            BBL_Address(bbl),
-            IARG_END
-        );
+  for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl)) {
+
+    BBL_InsertCall(bbl, IPOINT_ANYWHERE, (AFUNPTR)RecordBBL,
+                   IARG_FAST_ANALYSIS_CALL, IARG_INST_PTR,
+                   IARG_UINT32, BBL_Size(bbl), IARG_END);
+
+    if (!KnobMem)
+      continue;
+
+    for (INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins)) {
+
+      // instruments loads using a predicated call, i.e.
+      // the call happens iff the load will be actually executed
+
+      if (INS_IsMemoryRead(ins) && INS_IsStandardMemop(ins)) {
+        INS_InsertPredicatedCall(
+            ins, IPOINT_BEFORE, (AFUNPTR)RecordMem, IARG_INST_PTR, IARG_UINT32,
+            'R', IARG_MEMORYREAD_EA, IARG_MEMORYREAD_SIZE, IARG_END);
+      }
+
+      if (INS_HasMemoryRead2(ins) && INS_IsStandardMemop(ins)) {
+        INS_InsertPredicatedCall(
+            ins, IPOINT_BEFORE, (AFUNPTR)RecordMem, IARG_INST_PTR, IARG_UINT32,
+            'R', IARG_MEMORYREAD2_EA, IARG_MEMORYREAD_SIZE, IARG_END);
+      }
+
+      // instruments stores using a predicated call, i.e.
+      // the call happens iff the store will be actually executed
+      if (INS_IsMemoryWrite(ins) && INS_IsStandardMemop(ins)) {
+        INS_InsertPredicatedCall(
+            ins, IPOINT_BEFORE, (AFUNPTR)RecordWriteAddrSize,
+            IARG_MEMORYWRITE_EA, IARG_MEMORYWRITE_SIZE, IARG_END);
+
+        if (INS_HasFallThrough(ins)) {
+          INS_InsertCall(ins, IPOINT_AFTER, (AFUNPTR)RecordMemWrite,
+                         IARG_INST_PTR, IARG_END);
+        }
+        if (INS_IsBranchOrCall(ins)) {
+          INS_InsertCall(ins, IPOINT_TAKEN_BRANCH, (AFUNPTR)RecordMemWrite,
+                         IARG_INST_PTR, IARG_END);
+        }
+      }
     }
+  }
 }
 
 VOID Fini(INT32 code, VOID *v) {
-    *output_data = total;
+  *output_data = total;
+  TraceFile.close();
 }
-
 
 INT32 Usage() {
-    std::cerr << "This Pintool prints the execution trace of the target binary\n";
-    std::cerr << KNOB_BASE::StringKnobSummary() << std::endl;
-    return -1;
+  std::cerr << "This Pintool prints the execution trace of the target binary\n";
+  std::cerr << KNOB_BASE::StringKnobSummary() << std::endl;
+  return -1;
 }
-
 
 void output_init() {
-    output_data = (uint64_t*) mmap(NULL, kMaxOutput, PROT_WRITE, MAP_SHARED, kOutFd, 0);
-    close(kOutFd);
-    if (output_data == MAP_FAILED) {
-        std::cerr << "mmap of output failed" << std::endl;
-        exit(1);
-    }
-    output_pos = output_data;
-    write_output(0);
+  output_data =
+      (uint64_t *)mmap(NULL, kMaxOutput, PROT_WRITE, MAP_SHARED, kOutFd, 0);
+  close(kOutFd);
+  if (output_data == MAP_FAILED) {
+    std::cerr << "mmap of output failed" << std::endl;
+    exit(1);
+  }
+  output_pos = output_data;
+  write_output(0);
 }
 
+int main(int argc, char *argv[]) {
+  output_init();
 
-int main(int argc, char * argv[]) {
-    output_init();
+  PIN_InitSymbols();
+  if (PIN_Init(argc, argv))
+    return Usage();
 
-    PIN_InitSymbols();
-    if (PIN_Init(argc, argv)) return Usage();
+  if (KnobMem) {
+    TraceFile.open(KnobOutputFile.Value().c_str());
+  }
 
-    IMG_AddInstrumentFunction(ImageRoutine, 0);
+  IMG_AddInstrumentFunction(ImageRoutine, 0);
 
-    TRACE_AddInstrumentFunction(TraceRoutine, 0);
+  TRACE_AddInstrumentFunction(TraceRoutine, 0);
 
-    PIN_AddFiniFunction(Fini, 0);
+  PIN_AddFiniFunction(Fini, 0);
 
-    PIN_StartProgram();
+  PIN_StartProgram();
 
-    return 0;
+  return 0;
 }
