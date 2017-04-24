2c7b390dd6c54b4c8cdd147cd57720293f3bd0bd
rj2394@columbia.edu <rj2394@columbia.edu>
2017-04-24 02:29:29 -0400

    displayer: add slice displayer

---
+++ src/displayer/code.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"bytes"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+    "sort"
+)
+
+type File struct {
+	Name  string
+	Body  template.HTML
+	Lines int
+}
+
+type Slice struct {
+	Files []*File
+	Name  string
+}
+
+type FileArray []*File
+
+func (arr FileArray) Len() int {
+    return len(arr)
+}
+
+func (arr FileArray) Swap(i, j int) {
+    arr[i], arr[j] = arr[j], arr[i]
+}
+
+func (arr FileArray) Less(i, j int) bool {
+    return arr[i].Name < arr[j].Name
+}
+
+func parseFile(file string) ([][]byte, error) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	htmlReplacer := strings.NewReplacer(">", "&gt;", "<", "&lt;", "&", "&amp;", "\t", "    ")
+	var lines [][]byte
+	for {
+		idx := bytes.IndexByte(data, '\n')
+		if idx == -1 {
+			break
+		}
+		lines = append(lines, []byte(htmlReplacer.Replace(string(data[:idx]))))
+		data = data[idx+1:]
+	}
+	if len(data) != 0 {
+		lines = append(lines, data)
+	}
+	return lines, nil
+}
+
+func generateSliceHtml(w io.Writer, slice_file *SliceFile) error {
+	prefix := filepath.Join(os.Getenv("GOPATH"), "libs")
+
+	var slice Slice
+	slice.Name = slice_file.Name
+
+	for f, ls := range slice_file.Slice {
+		lines, err := parseFile(f)
+		if err != nil {
+			return err
+		}
+
+		var buf bytes.Buffer
+		cnt := 0
+		for n, l := range lines {
+			if cnt < len(ls) && n == ls[cnt]-1 {
+				buf.Write([]byte("<span class='in_slice'>"))
+				buf.Write(l)
+				buf.Write([]byte("</span>\n"))
+				cnt++
+			} else {
+				// buf.Write([]byte("<span class='not_in_slice'>"))
+				buf.Write(l)
+				buf.Write([]byte("\n"))
+				//				buf.Write([]byte("</span>\n"))
+			}
+
+		}
+		slice.Files = append(slice.Files, &File{
+			Name:  f[len(prefix):],
+			Body:  template.HTML(buf.String()),
+			Lines: len(ls),
+		})
+
+	}
+
+
+    sort.Sort(FileArray(slice.Files))
+	if err := sliceTemplate.Execute(w, slice); err != nil {
+		return nil
+	}
+	return nil
+}
+
+var sliceTemplate = template.Must(template.New("Slice").Parse(`
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<style>
+			body {
+				background: white;
+			}
+			#topbar {
+				background: black;
+				position: fixed;
+				top: 0; left: 0; right: 0;
+				height: 42px;
+				border-bottom: 1px solid rgb(70, 70, 70);
+			}
+			#nav {
+				float: left;
+				margin-left: 10px;
+				margin-top: 10px;
+			}
+			#content {
+				font-family: 'Courier New', Courier, monospace;
+				color: rgb(70, 70, 70);
+				margin-top: 50px;
+			}
+			.not_in_slice {
+				color: rgb(0, 0, 0);
+				font-weight: bold;
+			}
+			.in_slice {
+				color: rgb(255, 0, 0);
+				font-weight: bold;
+			}
+		</style>
+	</head>
+	<body>
+        <h>Slice: </h>
+        <br>
+		<div id="topbar">
+			<div id="nav">
+				<select id="files">
+				{{range $i, $f := .Files}}
+                <option value="file{{$i}}">{{$f.Name}}: {{$f.Lines}} </option>
+				{{end}}
+				</select>
+			</div>
+		</div>
+		<div id="content">
+		{{range $i, $f := .Files}}
+		<pre class="file" id="file{{$i}}" {{if $i}}style="display: none"{{end}}>{{$f.Body}}</pre>
+		{{end}}
+		</div>
+	</body>
+	<script>
+	(function() {
+		var files = document.getElementById('files');
+		var visible = document.getElementById('file0');
+		files.addEventListener('change', onChange, false);
+		function onChange() {
+			visible.style.display = 'none';
+			visible = document.getElementById(files.value);
+			visible.style.display = 'block';
+			window.scrollTo(0, 0);
+		}
+	})();
+	</script>
+</html>
+`))
---
+++ src/displayer/displayer.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+var (
+	flagDir     = flag.String("dir", "", "slices directory")
+	logger      = log.New(os.Stdout, "", 0)
+	slice_files []SliceFile
+)
+
+type SliceFile struct {
+	Name  string
+	Link  string
+	Slice map[string]([]int)
+}
+
+func slice(w http.ResponseWriter, r *http.Request) {
+	paths := strings.Split(r.URL.Path, "/")
+	if len(paths) != 3 {
+		logger.Printf("[ERROR] \tInvalid path: %v", r.URL.Path)
+		return
+	}
+	idx, err := strconv.Atoi(paths[2])
+	if err != nil {
+		logger.Printf("[ERROR] \t%v", err)
+		return
+	}
+
+	if err := load_slice(idx); err != nil {
+		logger.Printf("[ERROR] \t%v", err)
+		return
+	}
+
+	if err := generateSliceHtml(w, &slice_files[idx]); err != nil {
+		logger.Printf("[ERROR] \t%v", err)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if err := indexTemplate.Execute(w, slice_files); err != nil {
+		http.Error(w, fmt.Sprintf("failed to execute template: %v", err), http.StatusInternalServerError)
+	}
+}
+
+func load_slice(idx int) error {
+	if idx < 0 || idx >= len(slice_files) {
+		return fmt.Errorf("Slice index %d is out of range", idx)
+	}
+
+	if slice_files[idx].Slice != nil {
+		return nil
+	}
+
+	slice := &slice_files[idx]
+	logger.Printf("[INFO] \tLoading slice file %s", filepath.Join(*flagDir, slice.Name))
+	content, err := ioutil.ReadFile(filepath.Join(*flagDir, slice.Name))
+	if err != nil {
+		logger.Printf("[ERROR] \t%v", err)
+		return err
+	}
+
+	lines := strings.Split(string(content), "\n")
+	logger.Printf("[INFO] \tNumber of lines in slice = %v", len(lines)-1)
+	slice.Slice = make(map[string]([]int))
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		fields := strings.Split(line, ":")
+		if len(fields) != 2 {
+			logger.Printf("[ERROR] \tline = %v", line)
+			return fmt.Errorf("Unkown slice format")
+		}
+        f := fields[0]
+		fields[1] = strings.Split(fields[1], " ")[0]
+		l, err := strconv.ParseInt(fields[1], 10, 32)
+		if err != nil {
+			logger.Printf("[ERROR] \t%v", err)
+			return err
+		}
+		slice.Slice[f] = append(slice.Slice[f], int(l))
+	}
+	return nil
+}
+
+func setup() {
+	files, err := ioutil.ReadDir(*flagDir)
+	if err != nil {
+		logger.Panic("failed to read directory: ", *flagDir)
+	}
+
+	for idx, f := range files {
+		slice_files = append(slice_files, SliceFile{
+			Name:  f.Name(),
+			Link:  fmt.Sprintf("/%s/%d", "slice", idx),
+			Slice: nil,
+		})
+	}
+}
+
+func main() {
+	flag.Parse()
+	if *flagDir == "" {
+		logger.Println("Must specify a slice directory to view")
+		os.Exit(1)
+	}
+
+	setup()
+	http.HandleFunc("/", index)
+	http.HandleFunc("/slice/", slice)
+
+	if err := http.ListenAndServe(":5000", nil); err != nil {
+		logger.Panic("failed to start server on 5000")
+	}
+}
+
+var indexTemplate = template.Must(template.New("Index").Parse(addStyle(`
+<!doctype html>
+<html>
+<head>
+	<title>magisk</title>
+	{{STYLE}}
+</head>
+<body>
+<b>magisk displayer</b>
+<br>
+<br>
+
+<table>
+    <caption>Slice</caption>
+    {{range $s := $}}
+    <tr>
+        <td><a href="{{$s.Link}}">{{$s.Name}}</a></td>
+    </tr>
+    {{end}}
+</table>
+<br>
+</body></html>
+`)))
+
+func addStyle(html string) string {
+	return strings.Replace(html, "{{STYLE}}", htmlStyle, -1)
+}
+
+const htmlStyle = `
+	<style type="text/css" media="screen">
+		table {
+			border-collapse:collapse;
+			border:1px solid;
+		}
+		table caption {
+			font-weight: bold;
+		}
+		table td {
+			border:1px solid;
+			padding: 3px;
+		}
+		table th {
+			border:1px solid;
+			padding: 3px;
+		}
+		textarea {
+			width:100%;
+		}
+	</style>
+`
