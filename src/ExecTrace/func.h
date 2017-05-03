17bb87fc3c48156665d4d4c438c04708347a6384
HidyHan <yh2635@columbia.edu>
2017-05-03 14:11:00 -0400

    ExecTrace: helper headers

---
+++ src/ExecTrace/func.h
@@ -0,0 +1,72 @@
+#ifndef __FUNC_H
+#define __FUNC_H
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <vector>
+
+#include "pin.H"
+
+std::ofstream TraceFile;
+
+const int kMaxOutput = 1 << 24;
+uint64_t *output_data = NULL;
+uint64_t *output_pos = NULL;
+uint64_t total = 0;
+
+
+void write_output(uint64_t v) {
+  if (output_pos < output_data ||
+      (char *)output_pos >= (char *)output_data + kMaxOutput) {
+    std::cerr << "output overflowed" << std::endl;
+    return;
+  }
+  *output_pos = v;
+  output_pos++;
+}
+
+static VOID RecordMem(VOID *ip, CHAR r, VOID *addr, INT32 size) {
+  TraceFile << r << " " << ip << " " << addr << " " << size << endl;
+}
+
+static VOID *WriteAddr;
+static INT32 WriteSize;
+
+static VOID RecordWriteAddrSize(VOID *addr, INT32 size) {
+  WriteAddr = addr;
+  WriteSize = size;
+}
+
+static VOID RecordMemWrite(VOID *ip) {
+  RecordMem(ip, 'W', WriteAddr, WriteSize);
+}
+
+static VOID PIN_FAST_ANALYSIS_CALL RecordBBL(VOID *ip, UINT32 size) {
+  write_output((uint64_t)ip);
+  total++;
+  TraceFile << "B"
+          << " " << ip << " " << size << endl;
+}
+
+static VOID RecordExtCall(VOID *ip, const char* name, int count, ...) {
+  va_list args;
+  va_start(args, count); 
+  TraceFile << "E" << " " << ip << " " << name;
+  for (int i = 0; i <  count; i++) {
+    TraceFile << " " << va_arg(args, ADDRINT);
+  }
+  va_end(args);
+  TraceFile << "\n";
+}
+
+static VOID RecordCall(VOID *ip, ADDRINT target) {
+  TraceFile << "C" << " " << ip << " " << target << endl;
+}
+#endif
+
---
+++ src/ExecTrace/hooks.h
@@ -0,0 +1,74 @@
+#ifndef __HOOKS_H
+#define __HOOKS_H
+
+#include "func.h"
+
+VOID MallocBefore(VOID *ip, ADDRINT size) {
+  RecordBBL(ip, 0);
+  RecordExtCall(ip, "malloc", 1, size);
+}
+
+VOID MemcpyBefore(VOID *ip, ADDRINT dest, ADDRINT src, ADDRINT size) {
+  RecordBBL(ip, 0);
+  RecordExtCall(ip, "memcpy", 3, dest, src, size);
+  RecordMem(ip, 'R', (VOID*)src, size);
+  RecordMem(ip, 'W', (VOID*)dest, size);
+}
+
+VOID StrcpyBefore(VOID *ip, ADDRINT dest, ADDRINT src) {
+  INT32 size = strlen((const char *)src);
+
+  RecordBBL(ip, 0);
+  RecordExtCall(ip, "strcpy", 2, dest, src);
+  RecordMem(ip, 'R', (VOID*)src, size);
+  RecordMem(ip, 'W', (VOID*)dest, size);
+}
+
+VOID StrlenBefore(VOID *ip, ADDRINT src) {
+  RecordBBL(ip, 0);
+  RecordExtCall(ip, "strlen", 1, src);
+}
+
+VOID HookImage(IMG img) {
+  // hook special external functions
+  RTN mallocRtn = RTN_FindByName(img, "malloc@plt");
+  if (RTN_Valid(mallocRtn)) {
+    RTN_Open(mallocRtn);
+    RTN_InsertCall(mallocRtn, IPOINT_BEFORE, (AFUNPTR)MallocBefore,
+                   IARG_INST_PTR, IARG_UINT32, RTN_Size(mallocRtn),
+                   IARG_FUNCARG_CALLSITE_VALUE, 0, IARG_END);
+    RTN_Close(mallocRtn);
+  }
+
+  RTN strlenRtn = RTN_FindByName(img, "strlen@plt");
+  if (RTN_Valid(strlenRtn)) {
+    RTN_Open(strlenRtn);
+    RTN_InsertCall(strlenRtn, IPOINT_BEFORE, (AFUNPTR)StrlenBefore,
+                   IARG_INST_PTR, IARG_FUNCARG_CALLSITE_VALUE, 0, IARG_END);
+    RTN_Close(strlenRtn);
+  }
+
+  RTN memcpyRtn = RTN_FindByName(img, "memcpy@plt");
+  if (RTN_Valid(memcpyRtn)) {
+    RTN_Open(memcpyRtn);
+    RTN_InsertCall(memcpyRtn, IPOINT_BEFORE, (AFUNPTR)MemcpyBefore,
+                   IARG_INST_PTR, IARG_FUNCARG_CALLSITE_VALUE, 0,
+                   IARG_FUNCARG_CALLSITE_VALUE, 1, IARG_FUNCARG_CALLSITE_VALUE,
+                   2, IARG_END);
+    RTN_Close(memcpyRtn);
+  }
+
+  RTN strcpyRtn = RTN_FindByName(img, "strcpy@plt");
+  if (RTN_Valid(strcpyRtn)) {
+    RTN_Open(strcpyRtn);
+    RTN_InsertCall(strcpyRtn, IPOINT_BEFORE, (AFUNPTR)StrcpyBefore,
+                   IARG_INST_PTR, IARG_FUNCARG_CALLSITE_VALUE, 0,
+                   IARG_FUNCARG_CALLSITE_VALUE, 1, IARG_FUNCARG_CALLSITE_VALUE,
+                   2, IARG_END);
+    RTN_Close(strcpyRtn);
+  }
+}
+
+
+
+#endif
